e183ee2f288b1382d6b7fb77cdbebef14c7e5f9e
kamikazililian236 <l.kamikazi@alustudent.com>
2026-06-25 21:25:04 +0200

    finalisation

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,3 +1,4 @@
+  GNU nano 8.7.1                    1-last_digit.py                              
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
@@ -11,4 +12,4 @@ elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
     msg = f"Last digit of {number} is {last_digit}"
-    print(f"{msg} and is less than 6 and not 0") 
+    print(f"{msg} and is less than 6 and not 0")
