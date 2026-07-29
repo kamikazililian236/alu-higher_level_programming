2997d72485dac80e43b1d17ba87d63c1ce8c23c9
kamikazililian236 <l.kamikazi@alustudent.com>
2026-07-28 21:53:41 -0800

    Fix: PEP8 E501 line too long in 7-add_item.py docstring

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list, and saves them to a file."""
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
